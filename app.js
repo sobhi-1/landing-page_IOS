39d08b423ae80913065a963d11ca597acd68918d
Sobhi Mohamed <113847982+sobhi-1@users.noreply.github.com>
2022-10-04 04:54:31 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let img = document.getElementById("asd");
+let container = document.querySelector(".container");
+
+function phones(phone) {
+  img.src = phone;
+}
+
+function Colors(color) {
+  container.style.background = color;
+}
